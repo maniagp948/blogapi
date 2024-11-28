0241f380586ab8e6a63e6d91375481e5dc9b7600
Gabriel Mania <maniagp@proton.me>
2024-11-28 21:25:36 +0800

    Change URLs to routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path("users/", UserList.as_view()),  # type: ignore
-    path("users/<int:pk>/", UserDetail.as_view()),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+from .views import UserViewSet, PostViewSet
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+
+urlpatterns = router.urls
