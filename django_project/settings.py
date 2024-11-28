0815fa38db5ae4a1b8b41a79c0f8f5013a114661
Gabriel Mania <maniagp@proton.me>
2024-11-28 10:51:44 +0800

    Set permissions to authenticated only

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,9 +123,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-REST_FRAMEWORK = {  # new
+REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
