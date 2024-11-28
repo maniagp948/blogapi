3609c77f48de9cf14dc3386425d589efdf180b11
Gabriel Mania <maniagp@proton.me>
2024-11-28 22:29:54 +0800

    Dynamic schema

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
         "api/v1/dj-rest-auth/registration/",
         include("dj_rest_auth.registration.urls"),
     ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
